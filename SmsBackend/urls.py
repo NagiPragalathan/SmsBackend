e90624746e3c30f7f65c35f07a65fa49e40a9aff
nagipragalathan <kk2069037@gmail.com>
2023-10-12 02:29:46 +0530

    last commit

--- SmsBackend/urls.py
+++ SmsBackend/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/signup', user_registration),
-    path('/', welcome),
+    path('', welcome),
     path('api/logout', user_logout),
     path('api/login', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
